4260a381589cf5d3b1a9d61f392b25771515d3a2
David Samuelson <linksn.1fan@gmail.com>
2022-05-22 20:24:33 -0600

    finished thought routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -40,5 +40,52 @@ const thoughtController = {
             console.log(err);
             res.status(500).json(err);
         });
+    },
+
+    removeThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+        .then(deletedThought => {
+            if (!deletedThought) {
+                res.status(404).json({ message: 'No Thought with this ID was found' })
+                return;
+            }
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $pull: { thoughts: params.thoughtId }},
+                { new: true }
+            )
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No User with this ID was found' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: {reactions: { reactionId: params.reactionId }}},
+            { new: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No Thought with this ID was found' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     }
 }
+
+module.exports = thoughtController;
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+    addThought,
+    addReaction,
+    removeThought,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+router
+.route('/:userId')
+.post(addThought);
+
+router
+.route('/:userId/:thoughtId')
+.put(addReaction)
+.delete(removeThought);
+
+router
+.route('/:userId/:thoughtId/:reactionId')
+.delete(removeReaction);
+
+module.exports = router;
